b4d7e6e68a74d313860c846f64f9827e9149e2d1
Himani <hpatel8468@conestogac.on.ca>
2019-08-07 07:33:01 -0400

    bug fixed

--- app/src/main/java/com/appsforyou/hrincidentnew/ReportActivity.java
+++ app/src/main/java/com/appsforyou/hrincidentnew/ReportActivity.java
@@ -9,6 +9,6 @@ public class ReportActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_report2);
+        setContentView(R.layout.activity_report);
     }
 }
