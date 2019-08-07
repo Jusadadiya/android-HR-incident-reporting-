3fa208d0052573478c48f241d1450fb1e1f11998
Himani <hpatel8468@conestogac.on.ca>
2019-08-07 12:52:32 -0400

    Menu in Main

--- app/src/main/java/com/appsforyou/hrincidentnew/MainActivity.java
+++ app/src/main/java/com/appsforyou/hrincidentnew/MainActivity.java
@@ -3,6 +3,11 @@ package com.appsforyou.hrincidentnew;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,46 +16,31 @@ public class MainActivity extends AppCompatActivity {
     Button report, viewIncident;
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        getMenuInflater().inflate(R.menu.options, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+       int id = menuItem.getItemId();
+       if (id == R.id.report){
+           reportintent = new Intent(MainActivity.this, ReportActivity.class);
+            startActivity(reportintent);
+            return  false;
+       }
+       return super.onOptionsItemSelected(menuItem);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        report = (Button) findViewById(R.id.button);
-        viewIncident=(Button) findViewById(R.id.button2);
-        reportintent = new Intent(MainActivity.this, ReportActivity.class);
-        viewintent = new Intent(MainActivity.this, ViewActivity.class);
-        report.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(reportintent);
-            }
-        });
-        viewIncident.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(viewintent);
-            }
-        });
+
 
     }
 
 }
-   /*public boolean onCreateOptions(Menu menu){
-        getMenuInflater().inflate(R.menu.options,menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem menuItem){
-        Toast.makeText(this,"SelectedItem: "+menuItem.getTitle(),Toast.LENGTH_LONG).show();
-        switch (menuItem.getItemId()){
-            case R.id.report:
-                startActivity(reportintent);
-                return true;
-            case R.id.view:
-                startActivity(viewintent);
-                return true;
-                default:
-                    return super.onOptionsItemSelected(menuItem);
-        }
-    }
-}*/
+
+
