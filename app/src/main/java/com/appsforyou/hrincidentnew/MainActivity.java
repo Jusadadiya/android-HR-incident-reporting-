d4ceec64f81dba842df84aeceaa1f538d1f2c089
Himani <hpatel8468@conestogac.on.ca>
2019-08-07 07:27:47 -0400

    layout of app updated and created

--- app/src/main/java/com/appsforyou/hrincidentnew/MainActivity.java
+++ app/src/main/java/com/appsforyou/hrincidentnew/MainActivity.java
@@ -2,13 +2,38 @@ package com.appsforyou.hrincidentnew;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+Intent reportintent,viewintent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         reportintent = new Intent(MainActivity.this,ReportActivity.class);
+         viewintent = new Intent(MainActivity.this,ViewActivity.class);
+    }
+
+   public boolean onCreateOptions(Menu menu){
+        getMenuInflater().inflate(R.menu.options,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        Toast.makeText(this,"SelectedItem: "+menuItem.getTitle(),Toast.LENGTH_LONG).show();
+        switch (menuItem.getItemId()){
+            case R.id.report:
+                startActivity(reportintent);
+                return true;
+            case R.id.view:
+                startActivity(viewintent);
+                return true;
+                default:
+                    return super.onOptionsItemSelected(menuItem);
+        }
     }
 }
