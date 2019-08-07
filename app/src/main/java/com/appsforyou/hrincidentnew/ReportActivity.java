a7dbe1ec6bff44ccf1b6457e6ed1bd50b5870769
Jay Usadadiya <jusadadiya2356@conestogac.on.ca>
2019-08-07 08:19:34 -0400

    date set to current date

--- app/src/main/java/com/appsforyou/hrincidentnew/ReportActivity.java
+++ app/src/main/java/com/appsforyou/hrincidentnew/ReportActivity.java
@@ -1,8 +1,12 @@
 package com.appsforyou.hrincidentnew;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ReportActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class ReportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+        getCurrentDate();
+
+
+    }
+    public void getCurrentDate() {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat mdformat = new SimpleDateFormat("MM / dd / yyyy ");
+        String strDate = mdformat.format(calendar.getTime());
+        display(strDate);
+    }
+
+    private void display(String num) {
+        EditText datetext = (EditText) findViewById(R.id.editText2);
+        datetext.setEnabled(false);
+        datetext.setText(num);
     }
 }
