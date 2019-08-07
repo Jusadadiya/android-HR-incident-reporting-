f7170480d3f082973327e5275b79ba69553e4532
Jay Usadadiya <jusadadiya2356@conestogac.on.ca>
2019-08-07 11:43:31 -0400

    autofill employee table complete

--- app/src/main/java/com/appsforyou/hrincidentnew/ReportActivity.java
+++ app/src/main/java/com/appsforyou/hrincidentnew/ReportActivity.java
@@ -1,6 +1,8 @@
 package com.appsforyou.hrincidentnew;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -9,6 +11,9 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,8 +29,78 @@ public class ReportActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
 
-        EditText empId = (EditText) findViewById(R.id.empnumEdit);
-        EditText empName = (EditText) findViewById(R.id.nameEdit);
+        final EditText empId = (EditText) findViewById(R.id.empnumEdit);
+        final EditText empName = (EditText) findViewById(R.id.nameEdit);
+        final EditText dept = (EditText) findViewById(R.id.departEdit);
+        final EditText position = (EditText) findViewById(R.id.positionEdit);
+        empName.setEnabled(false);
+        dept.setEnabled(false);
+        position.setEnabled(false);
+
+        // this listener will autofill employee details from database based on user input of employee ID
+        empId.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                try {
+                    if (Integer.parseInt(empId.getText().toString()) > 0 && Integer.parseInt(empId.getText().toString()) < 6) {
+                        Cursor c = db.rawQuery("SELECT empName,empDep,empPosition FROM tbl_Employee WHERE empID=" + Integer.parseInt(empId.getText().toString()) + "", null);
+
+                        if (c.moveToFirst()) {
+                            do {
+                                // Passing values
+                                String name = c.getString(0);
+                                String empDep = c.getString(1);
+                                String empPosition = c.getString(2);
+                                // setting values to textfields
+                                empName.setText(name);
+                                dept.setText(empDep);
+                                position.setText(empPosition);
+                            } while (c.moveToNext());
+                        }
+                    } else {
+                        Toast.makeText(ReportActivity.this, "Invalid ID Please Try Again!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                catch(Exception e1) {
+                    e1.printStackTrace();
+                    Log.e("Memory exceptions","exceptions"+e1);
+                }
+            }
+
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                try {
+                    if (Integer.parseInt(empId.getText().toString()) > 0 && Integer.parseInt(empId.getText().toString()) < 6) {
+                        Cursor c = db.rawQuery("SELECT empName,empDep,empPosition FROM tbl_Employee WHERE empID=" + Integer.parseInt(empId.getText().toString()) + "", null);
+
+                        if (c.moveToFirst()) {
+                            do {
+                                // Passing values
+                                String name = c.getString(0);
+                                String empDep = c.getString(1);
+                                String empPosition = c.getString(2);
+                                // setting values to textfields
+                                empName.setText(name);
+                                dept.setText(empDep);
+                                position.setText(empPosition);
+                            } while (c.moveToNext());
+                        }
+                    } else {
+                        Toast.makeText(ReportActivity.this, "Invalid ID Please Try Again!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                catch(Exception e1) {
+                    e1.printStackTrace();
+                    Log.e("Memory exceptions","exceptions"+e1);
+                }
+            }
+        });
 
         getCurrentDate();
         createDb();
@@ -34,20 +109,15 @@ public class ReportActivity extends AppCompatActivity {
         db.execSQL("CREATE TABLE IF NOT EXISTS tbl_Employee(empID INTEGER PRIMARY KEY AUTOINCREMENT,empName VARCHAR,empDep VARCHAR,empPosition VARCHAR)");
         db.execSQL("INSERT into tbl_Employee(empName,empDep,empPosition) values('Jay','IT','Project Manager'),('William','IT','Tester')," +
                 "('Wen','Management','Manager'),('Himani','Management','Associate'),('Khushi','Customer Service','Head Of Department')");
-        /* View all data from database*/
 
+        /* View data from database*/
         report.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText empId = (EditText) findViewById(R.id.empnumEdit);
-                EditText empName = (EditText) findViewById(R.id.nameEdit);
 
-               /* Cursor curs1=db.rawQuery("SELECT * FROM tbl_Employee WHERE empID="+Integer.parseInt(empId.getText().toString())+"",null);
-                Toast.makeText(ReportActivity.this,"Hi, ",Toast.LENGTH_SHORT).show();
-                // empName.setText(curs1.getString(curs1.getColumnIndex("empName")));
-                Toast.makeText(ReportActivity.this,"Hi, "+curs1.getString(0),Toast.LENGTH_SHORT).show();
 
-                curs1.close();*/
+
+                db.close();
             }
         });
     }
