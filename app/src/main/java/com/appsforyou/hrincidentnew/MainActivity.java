a6baf4c2017ef538c26d27af7dfb4399348009ec
Jay Usadadiya <jusadadiya2356@conestogac.on.ca>
2019-08-07 08:04:12 -0400

    Main screen coded

--- app/src/main/java/com/appsforyou/hrincidentnew/MainActivity.java
+++ app/src/main/java/com/appsforyou/hrincidentnew/MainActivity.java
@@ -1,27 +1,43 @@
 package com.appsforyou.hrincidentnew;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-Intent reportintent,viewintent;
+    Intent reportintent, viewintent;
+    Button report, viewIncident;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         reportintent = new Intent(MainActivity.this,ReportActivity.class);
-         viewintent = new Intent(MainActivity.this,ViewActivity.class);
+        report = (Button) findViewById(R.id.button);
+        viewIncident=(Button) findViewById(R.id.button2);
+        reportintent = new Intent(MainActivity.this, ReportActivity.class);
+        viewintent = new Intent(MainActivity.this, ViewActivity.class);
+        report.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(reportintent);
+            }
+        });
+        viewIncident.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(viewintent);
+            }
+        });
     }
 
-   public boolean onCreateOptions(Menu menu){
+}
+   /*public boolean onCreateOptions(Menu menu){
         getMenuInflater().inflate(R.menu.options,menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem menuItem){
         Toast.makeText(this,"SelectedItem: "+menuItem.getTitle(),Toast.LENGTH_LONG).show();
@@ -36,4 +52,4 @@ Intent reportintent,viewintent;
                     return super.onOptionsItemSelected(menuItem);
         }
     }
-}
+}*/
