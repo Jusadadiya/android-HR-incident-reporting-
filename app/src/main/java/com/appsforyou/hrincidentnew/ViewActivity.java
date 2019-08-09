d8489333155f07c0afb0e36cb8cc05e94272d3d0
orlandowww <orlandowww@163.com>
2019-08-09 08:17:06 -0400

    fix View activity

--- app/src/main/java/com/appsforyou/hrincidentnew/ViewActivity.java
+++ app/src/main/java/com/appsforyou/hrincidentnew/ViewActivity.java
@@ -1,6 +1,5 @@
 package com.appsforyou.hrincidentnew;
 
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -9,6 +8,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -49,24 +49,14 @@ public class ViewActivity extends AppCompatActivity {
         // Retrieve all data from tbl_IncidentHistory table
         Cursor csl = db.rawQuery("SELECT * FROM tbl_IncidentHistory", null);
         if (csl.getCount() == 0){
-            showStatus("Error", "No Data Found!");
+            Toast.makeText(getApplicationContext(), "Incident History Is Empty", Toast.LENGTH_LONG).show();
             return;
         }
         StringBuffer bfr;
 
         // Call the procedure to show all records
         bfr = showRecords(csl);
-        showStatus("Data", bfr.toString());
         csl.close();
-    }
-
-    // Procedure to build message string
-    public void showStatus(String title, String resultContent){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setCancelable(true);
-        builder.setTitle(title);
-        builder.setMessage(resultContent);
-        builder.show();
     }
 
     // procedure to show records
