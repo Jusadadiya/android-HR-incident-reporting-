bf2dfa91c7c83b85d1b04401e0c6982e88cb6500
orlandowww <orlandowww@163.com>
2019-08-08 22:32:32 -0400

    delete a definition

--- app/src/main/java/com/appsforyou/hrincidentnew/MainActivity.java
+++ app/src/main/java/com/appsforyou/hrincidentnew/MainActivity.java
@@ -1,19 +1,15 @@
 package com.appsforyou.hrincidentnew;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     Intent reportintent, viewintent;
-    Button report, viewIncident;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -41,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
     }
 
